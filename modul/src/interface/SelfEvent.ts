968406fc8042dac7927f4e8b56a2a16984b682f2
Ysb <15810856489@163.com>
2018-09-06 16:18:49 +0800

    添加说明

---
+++ modul/src/interface/SelfEvent.ts
@@ -0,0 +1,15 @@
+interface ICanvasInfo{
+    top: number;
+    left: number;
+    bottom: number;
+    right: number;
+    width: number;
+    height: number;
+}
+
+
+export interface ISelfEvent{
+    mouseX: number;
+    mouseY: number;
+    canvasInFo: ICanvasInfo;
+}
